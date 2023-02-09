e344dc4242992e60cb8b63dfde0a66dde2c7ff7b
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-02-09 18:35:27 +0300

    Deletes the reverse unit testing file and adds its contents inside solver_spec.rb

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../solver'
 
 describe Solver do
+  context "Factorial Function Unit Testing"
   it 'Should return 120' do
     solver = Solver.new
     result = solver.factorial(5)
@@ -18,3 +19,24 @@ describe Solver do
     expect(result).to eq(40_320)
   end
 end
+
+describe Solver do
+  context "Reverse Function Unit Testing"
+  it 'Should return trem' do
+    solver = Solver.new
+    result = solver.reverse("mert")
+    expect(result).to eq("trem")
+  end
+
+  it 'Should return missaw' do
+    solver = Solver.new
+    result = solver.reverse("wassim")
+    expect(result).to eq("missaw")
+  end
+  it 'Should return tsurt' do
+    solver = Solver.new
+    result = solver.reverse("trust")
+    expect(result).to eq("tsurt")
+  end
+end
+
