a90aa31934596d51bf47964641e0e949344ca3e8
Trust7 <simeontrust7@gmail.com>
2023-02-10 01:15:09 +0100

    Add tests for fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -39,3 +39,30 @@ describe Solver do
     expect(result).to eq('tsurt')
   end
 end
+
+describe Solver do
+  context 'fizzbuzz method Unit Testing'
+  it 'Should return fizz' do
+    solver = Solver.new
+    result = solver.fizzbuzz(12)
+    expect(result).to eq('fizz')
+  end
+
+  it 'Should return buzz' do
+    solver = Solver.new
+    result = solver.fizzbuzz(10)
+    expect(result).to eq('buzz')
+  end
+
+  it 'Should return fizzbuzz' do
+    solver = Solver.new
+    result = solver.fizzbuzz(15)
+    expect(result).to eq('fizzbuzz')
+  end
+  
+  it 'Should return "7"' do
+    solver = Solver.new
+    result = solver.fizzbuzz(7)
+    expect(result).to eq('7')
+  end
+end
